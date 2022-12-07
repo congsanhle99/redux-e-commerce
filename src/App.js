253d38a792a3b61f68c4a60ba797f5515791c1d5
CongSanhLe <congsanhle1999@gmail.com>
2022-12-08 05:23:51 +0700

    [08122022] Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import Home from "./pages/home/Home";
-import Contact from "./pages/contact/Contact";
+// Pages
+import { Home, Contact } from "./pages/index";
+// Component
+import { Header, Footer } from "./components/index";
+
 function App() {
   return (
     <>
