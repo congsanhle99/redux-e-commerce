ca83634b1c317f35b79fc4964c844a7db5502323
CongSanhLe <congsanhle1999@gmail.com>
2022-12-08 06:13:45 +0700

    [08122022] auth Reset Password Page

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import forgotImg from "../../assets/eshop-images/forgot.png";
+import Card from "../../components/card/Card";
+import styles from "./auth.module.scss";
 
 const Reset = () => {
-  return <div>Reset</div>;
+  return (
+    <>
+      <section className={`container ${styles.auth}`}>
+        <div className={styles.img}>
+          <img src={forgotImg} alt="loginImg" width="400" />
+        </div>
+        <Card>
+          <div className={styles.form}>
+            <h2>Reset Password</h2>
+            <form action="">
+              <input required type="email" placeholder="Email" />
+              <button className="--btn --btn-primary --btn-block">Reset Password</button>
+              <div className={styles.links}>
+                <p>
+                  <Link to="/login">Login -</Link>
+                </p>
+                <p>
+                  <Link to="/register">- Register</Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </Card>
+      </section>
+    </>
+  );
 };
 
 export default Reset;
