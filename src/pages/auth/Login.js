0f092818cd54a8e476746b06928b7a49f36b353e
CongSanhLe <congsanhle1999@gmail.com>
2022-12-08 05:59:29 +0700

    [08122022] Card comp

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -3,31 +3,34 @@ import styles from "./auth.module.scss";
 import loginImg from "../../assets/eshop-images/login.png";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
+import Card from "../../components/card/Card";
 const Login = () => {
   return (
     <section className={`container ${styles.auth}`}>
       <div className={styles.img}>
         <img src={loginImg} alt="loginImg" width="400" />
       </div>
-      <div className={styles.form}>
-        <h2>Login</h2>
-        <form action="">
-          <input required type="email" placeholder="Email" />
-          <input required type="password" placeholder="Password" />
-          <button className="--btn --btn-primary --btn-block">Login</button>
-          <div className={styles.links}>
-            <Link to="/reset">Reset Password</Link>
-          </div>
-          <p>-- or --</p>
-        </form>
-        <button className="--btn --btn-danger --btn-block">
-          <FaGoogle color="#fff" /> Login With Google
-        </button>
-        <span className={styles.register}>
-          <p>Don't have account?</p>
-          <Link to="/register">Register</Link>
-        </span>
-      </div>
+      <Card>
+        <div className={styles.form}>
+          <h2>Login</h2>
+          <form action="">
+            <input required type="email" placeholder="Email" />
+            <input required type="password" placeholder="Password" />
+            <button className="--btn --btn-primary --btn-block">Login</button>
+            <div className={styles.links}>
+              <Link to="/reset">Reset Password</Link>
+            </div>
+            <p>-- or --</p>
+          </form>
+          <button className="--btn --btn-danger --btn-block">
+            <FaGoogle color="#fff" /> Login With Google
+          </button>
+          <span className={styles.register}>
+            <p>Don't have account?</p>
+            <Link to="/register">Register</Link>
+          </span>
+        </div>
+      </Card>
     </section>
   );
 };
