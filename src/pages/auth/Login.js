f0bafa1c1d8f3cb9fb82f9e5943d99902ff95c7d
CongSanhLe <congsanhle1999@gmail.com>
2022-12-08 16:40:13 +0700

    [08122022] Login with firebase

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,37 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./auth.module.scss";
 import loginImg from "../../assets/eshop-images/login.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import Card from "../../components/card/Card";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { toast, ToastContainer } from "react-toastify";
+import Loader from "../../components/loader/Loader";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        setIsLoading(false);
+        toast.success("Login successful");
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        toast.error(`${errorMessage} - ${errorCode}`);
+        setIsLoading(false);
+      });
+  };
+
   return (
-    <section className={`container ${styles.auth}`}>
-      <div className={styles.img}>
-        <img src={loginImg} alt="loginImg" width="400" />
-      </div>
-      <Card>
-        <div className={styles.form}>
-          <h2>Login</h2>
-          <form action="">
-            <input required type="email" placeholder="Email" />
-            <input required type="password" placeholder="Password" />
-            <button className="--btn --btn-primary --btn-block">Login</button>
-            <div className={styles.links}>
-              <Link to="/reset">Reset Password</Link>
-            </div>
-            <p>-- or --</p>
-          </form>
-          <button className="--btn --btn-danger --btn-block">
-            <FaGoogle color="#fff" /> Login With Google
-          </button>
-          <span className={styles.register}>
-            <p>Don't have account?</p>
-            <Link to="/register">Register</Link>
-          </span>
+    <>
+      {isLoading && <Loader />}
+      <section className={`container ${styles.auth}`}>
+        <div className={styles.img}>
+          <img src={loginImg} alt="loginImg" width="400" />
         </div>
-      </Card>
-    </section>
+        <Card>
+          <div className={styles.form}>
+            <h2>Login</h2>
+            <form onSubmit={loginUser}>
+              <input
+                required
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                required
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit" className="--btn --btn-primary --btn-block">
+                Login
+              </button>
+              <div className={styles.links}>
+                <Link to="/reset">Reset Password</Link>
+              </div>
+              <p>-- or --</p>
+            </form>
+            <button className="--btn --btn-danger --btn-block">
+              <FaGoogle color="#fff" /> Login With Google
+            </button>
+            <span className={styles.register}>
+              <p>Don't have account?</p>
+              <Link to="/register">Register</Link>
+            </span>
+          </div>
+        </Card>
+      </section>
+    </>
   );
 };
 
